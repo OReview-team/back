1e7c0f3eb0e12749ae3c247d722c7e8991b897ce
Morvoren Kim <morvoren.kim@gmail.com>
2025-03-16 02:23:08 +0900

    fix(): jwt strategy 새로 발급된 토큰 res.cookie로 처리하는 중

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -1,55 +1,110 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Res, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import type { Request } from 'express';
+import type { Request, Response as ExpressResponse } from 'express';
 import { Strategy } from 'passport-jwt';
+import jwt from 'jsonwebtoken';
 
-import { TokenType } from '../../constants/token-type.ts';
+// import { TokenType } from '../../constants/token-type.ts';
 import { ApiConfigService } from '../../shared/services/api-config.service.ts';
 import type { UserEntity } from '../user/entities/user.entity.ts';
 import { UserService } from '../user/user.service.ts';
 import { AuthService } from './auth.service.ts';
+import type {
+  IAccessTokenPayload,
+  IRefreshTokenPayload,
+} from './dto/refresh-token.interface.ts';
+import { setAuthCookies } from './utils/cookie.utils.ts';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
-    configService: ApiConfigService,
+    private configService: ApiConfigService,
     private authService: AuthService,
     private userService: UserService,
   ) {
     super({
-      jwtFromRequest: (req) => (req as Request).cookies.accessToken as string,
+      // passport는 jwtFromRequest와 secretOrKey가 필수이기 때문에 여기서 access token을 추출할 경우
+      // access token이 만료되면 잡을 방법이 없어 해당 로직에서 refreshToken을 잡는다.
+      jwtFromRequest: (req) => (req as Request).cookies.refreshToken as string,
+      ignoreExpiration: false,
       secretOrKey: configService.authConfig.publicKey,
+      passReqToCallback: true,
     });
   }
 
   async validate(
-    args: { userId: Uuid; type: TokenType },
-    req: Request,
-  ): Promise<UserEntity> {
-    if (args.type === TokenType.ACCESS_TOKEN) {
-      const user = await this.userService.findOne({
-        id: args.userId as never,
-      });
+    args: { cookies: { accessToken: string; refreshToken: string } },
+    req: any,
+    @Res() res: ExpressResponse,
+  ): Promise<void> {
+    let user: UserEntity | null = null;
+    try {
+      // 1. access token 검증
+      const accessToken = args.cookies.accessToken as string;
 
-      if (!user) {
-        throw new UnauthorizedException();
+      let accessTokenPayload: IAccessTokenPayload;
+
+      try {
+        accessTokenPayload = jwt.verify(
+          accessToken,
+          this.configService.authConfig.publicKey,
+        ) as IAccessTokenPayload;
+
+        user = await this.userService.findOne({
+          id: accessTokenPayload.userId as never,
+        });
+
+        if (user) {
+          req.user = user;
+        }
+      } catch (error: any) {
+        if (error.name === 'TokenExpiredError') {
+          // 2. access token 만료된 경우 여기서 refresh token 검증
+          const refreshToken = args.cookies.refreshToken as string;
+
+          if (!refreshToken) {
+            throw new UnauthorizedException(
+              'refresh token이 존재하지 않습니다.',
+            );
+          }
+
+          const refreshTokenPayload = jwt.verify(
+            refreshToken,
+            this.configService.authConfig.privateKey,
+          ) as IRefreshTokenPayload;
+
+          if (refreshTokenPayload.expiresAt < new Date()) {
+            throw new UnauthorizedException(
+              'refresh token이 만료되었습니다. 다시 로그인해주세요.',
+            );
+          }
+
+          user = await this.userService.findOne({
+            id: refreshTokenPayload.userId,
+          });
+
+          if (user) {
+            req.user = user;
+
+            // 3. refresh token 검증에 성공했다면 access token과 refresh token 모두 재 발급
+            const result = await this.authService.createJwtToken(user);
+
+            (res as ExpressResponse).cookie('accessToken', result.accessToken, {
+              httpOnly: true,
+            });
+            (res as ExpressResponse).cookie(
+              'refreshToken',
+              result.refreshToken,
+              { httpOnly: true },
+            );
+          }
+        }
       }
-
-      return user;
+    } catch (error: any) {
+      throw new UnauthorizedException();
     }
-
-    const refreshToken = req.cookies.refreshToken as string;
-
-    if (!refreshToken) {
-      throw new UnauthorizedException('refresh token이 존재하지 않습니다.');
+    if (!user) {
+      throw new UnauthorizedException();
     }
-
-    const result = await this.authService.regenerateAccessToken(refreshToken);
-
-    req.headers['access-token'] = result.accessToken;
-
-    req.headers['refresh-token'] = result.refreshToken;
-
-    return result.user;
   }
 }
