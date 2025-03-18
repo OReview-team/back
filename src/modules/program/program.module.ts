ba3fd317be90c5d5f73acb0764b096fdabf09995
Morvoren Kim <morvoren.kim@gmail.com>
2025-03-18 15:53:16 +0900

    add(): module에 각 엔터티 추가

--- src/modules/program/program.module.ts
+++ src/modules/program/program.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { GenreEntity } from './entities/genre.entity.ts';
 import { ProgramEntity } from './entities/program.entity.ts';
+import { WatchProviderEntity } from './entities/watch-provider.entity.ts';
 import { ProgramController } from './program.controller.ts';
 import { ProgramService } from './program.service.ts';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProgramEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProgramEntity, GenreEntity, WatchProviderEntity]),
+  ],
   controllers: [ProgramController],
   providers: [ProgramService],
 })
