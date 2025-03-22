bf641ec1037f2c8ddc26df17cb175f424a5503e6
Morvoren Kim <morvoren.kim@gmail.com>
2025-03-22 16:40:55 +0900

    add():program module에 TMDB관련 환경변수 추가

--- src/modules/program/program.module.ts
+++ src/modules/program/program.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { GenreEntity } from './entities/genre.entity.ts';
@@ -12,6 +13,30 @@ import { ProgramService } from './program.service.ts';
     TypeOrmModule.forFeature([ProgramEntity, GenreEntity, WatchProviderEntity]),
   ],
   controllers: [ProgramController],
-  providers: [ProgramService],
+  providers: [
+    ProgramService,
+    {
+      provide: 'TMDB_CONFIG',
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const tmdbApiKey: string =
+          configService.get<string>('TMDB_API_KEY') ?? '';
+
+        const tmdbUrl: string =
+          configService.get<string>('TMDB_URL_BASE') ?? '';
+
+        if (!tmdbApiKey || !tmdbUrl) {
+          throw new Error(
+            '환경 변수 입력이 되지 않았습니다. : TMDB_API_KEY or TMDB_URL_BASE',
+          );
+        }
+
+        return {
+          tmdbApiKey,
+          tmdbUrl,
+        };
+      },
+    },
+  ],
 })
 export class ProgramModule {}
