ab811006c10e03cc7a809f5b6bb2f04aa3a8bb73
Morvoren Kim <morvoren.kim@gmail.com>
2025-03-29 18:10:47 +0900

    fix(): watch provider entity 속성명 재작성

--- src/modules/program/entities/watch-provider.entity.ts
+++ src/modules/program/entities/watch-provider.entity.ts
@@ -9,10 +9,13 @@ import { ProgramEntity } from './program.entity.ts';
 @UseDto(WatchProviderDto)
 export class WatchProviderEntity extends AbstractEntity<WatchProviderDto> {
   @Column({ type: 'int', comment: 'TMDB에서 제공하는 provider_id' })
-  originId!: number;
+  tmdbProviderId!: number;
 
   @Column({ type: 'varchar' })
   name!: string;
+
+  @Column({ type: 'varchar' })
+  logoPath!: string;
 
   @ManyToMany(() => ProgramEntity, (program) => program.watchProviders)
   programs!: ProgramEntity[];
